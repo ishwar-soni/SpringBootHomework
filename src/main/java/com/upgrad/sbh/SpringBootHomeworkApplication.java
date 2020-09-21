282112e0171d33b69c4710e0f7fe328c912d2489
Ishwar Soni <ishwar.soni@upgrad.com>
2020-09-21 21:48:24 +0530

    TODO 5 Providing different application.properties based on profile

--- src/main/java/com/upgrad/sbh/SpringBootHomeworkApplication.java
+++ src/main/java/com/upgrad/sbh/SpringBootHomeworkApplication.java
@@ -3,6 +3,8 @@ package com.upgrad.sbh;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 
 import javax.sql.DataSource;
 
@@ -12,6 +14,13 @@ public class SpringBootHomeworkApplication {
 	public static void main(String[] args) {
 		ApplicationContext context = SpringApplication.run(SpringBootHomeworkApplication.class, args);
 		DataSource dataSource = context.getBean(DataSource.class);
+
+		System.out.println("**********************************");
+		Resource resourceDev = new ClassPathResource("application-dev.properties");
+		Resource resourceTesting = new ClassPathResource("application-testing.properties");
+		Resource resourceProd = new ClassPathResource("application-prod.properties");
+		System.out.println(resourceDev.exists() && resourceTesting.exists() && resourceProd.exists());
+		System.out.println("**********************************");
 	}
 
 }
