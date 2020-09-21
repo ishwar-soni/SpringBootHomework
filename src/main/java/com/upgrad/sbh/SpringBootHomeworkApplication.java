24af77369320d118a4fd5bc5a2dfc6973ad00716
Ishwar Soni <ishwar.soni@upgrad.com>
2020-09-21 17:52:14 +0530

    TODO 1: Running application jar file

---
+++ src/main/java/com/upgrad/sbh/SpringBootHomeworkApplication.java
@@ -0,0 +1,13 @@
+package com.upgrad.sbh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootHomeworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootHomeworkApplication.class, args);
+	}
+
+}
